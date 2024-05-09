46af14b7fd6dce1387bcd57414435897da8e44b4
Lee-Hacksun <hacksun530@hs.ac.kr>
2024-05-09 22:26:52 +0900

    샛별문고 데이터베이스에 대한 기본 뼈대 작업

---
+++ morningstar-library/src/constant/ManagerOrderAttribute.java
@@ -0,0 +1,10 @@
+ // ManagerOrder테이블의 에트리뷰트에 접근하기 위한 이름 목록
+ // 발주ID
+ // 발주일
+ // 발주 도서목록
+
+package constant;
+
+public class ManagerOrderAttribute {
+
+}
---
+++ morningstar-library/src/service/RegsiterService.java
@@ -0,0 +1,7 @@
+ // 회원 등록에 대한 기능을 다루는 클래스 
+
+package service;
+
+public class RegsiterService {
+
+}
