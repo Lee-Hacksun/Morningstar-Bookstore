2d19568b376a28480a55aa5fd165dec2e62258e3
Lee-Hacksun <hacksun530@hs.ac.kr>
2024-05-11 00:42:26 +0900

    데이터베이스 연결 구현

--- morningstar-library/src/constant/DBConfig.java
+++ morningstar-library/src/constant/DBConfig.java
@@ -1,5 +1,8 @@
 package constant;
 
 public class DBConfig {
-
+	public static final String DRIVER = "com.mysql.cj.jdbc.Driver"; 
+	public static final String URL = "jdbc:mysql://localhost:3306/morningstar_bookstore";
+	public static final String ADMIN_ID = "root";
+	public static final String ADMIN_PW = "onlyroot";
 }
--- morningstar-library/src/database/DBConnector.java
+++ morningstar-library/src/database/DBConnector.java
@@ -1,7 +1,37 @@
- // 데이터베이스와의 연결 Connection 객체를 반환하는 클래스 
-
 package database;
 
-public class DBConnector {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+import constant.DBConfig;
+
+public class DBConnector {
+	public static Connection con = null;
+	
+	static {
+		try {
+			Class.forName(DBConfig.DRIVER);
+			con = DriverManager.getConnection(DBConfig.URL, DBConfig.ADMIN_ID, DBConfig.ADMIN_PW);
+		}
+		catch (java.lang.ClassNotFoundException ex){
+			System.err.println("Driver load erro in DBConnector class" + ex.getMessage());
+		}
+		catch (SQLException ex) {
+			System.err.println("Database connection erro in DBConnector class" + ex.getMessage());
+		}
+	}
+	
+	public static Connection getConnection() {
+		try {
+			if (con == null | con.isClosed()) {
+				con = DriverManager.getConnection(DBConfig.URL, DBConfig.ADMIN_ID, DBConfig.ADMIN_PW);
+			}
+		}
+		catch (SQLException ex) {
+			System.err.println("Database connection erro in DBConnector class" + ex.getMessage());
+		}
+		
+		return con;
+	}
 }
