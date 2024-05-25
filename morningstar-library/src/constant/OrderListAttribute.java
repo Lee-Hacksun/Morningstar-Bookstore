0c9d299059f2147163d6c0cc73e8ae1a8a34316b
Lee-Hacksun <hacksun530@hs.ac.kr>
2024-05-25 23:10:29 +0900

    주문 추가 및 주문 불러오기 기능 일부 구현

--- morningstar-library/src/constant/OrderListAttribute.java
+++ morningstar-library/src/constant/OrderListAttribute.java
@@ -2,9 +2,9 @@ package constant;
 
 public class OrderListAttribute {
 	
-    public static final String ORDER_LIST_TABLE_NAME = "orderList";
-    public static final String ORDER_LIST_ORDER_ID = "orderID";
-    public static final String ORDER_LIST_ISBN = "ISBN";
-    public static final String ORDER_LIST_COUNT = "count";
+    public static final String TABLE_NAME = "orderList";
+    public static final String ORDER_ID = "orderID";
+    public static final String ISBN = "ISBN";
+    public static final String COUNT = "count";
 
 }
--- morningstar-library/src/service/OrderService.java
+++ morningstar-library/src/service/OrderService.java
@@ -1,7 +1,142 @@
- // 주문에 대한 데이터베이스 작업을 다루는 클래스
-
 package service;
 
-public class OrderService {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.Vector;
 
+import constant.OrderAttribute;
+import constant.OrderListAttribute;
+import database.DBConnector;
+import model.Book;
+import model.Cart;
+import model.Order;
+import model.Pair;
+
+public class OrderService {
+	private Connection con;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	
+	private int orderID;
+	private Vector<Order> orders;
+	
+	public OrderService() {
+		try {
+			pstmt = con.prepareStatement("SELECT MAX(orderID) AS orderID FROM "+ OrderAttribute.TABLE_NAME +";");
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				orderID = rs.getInt("orderID");
+			}
+		} catch (SQLException ex) {
+			System.err.println("Database error in OrderService" + ex.getMessage());
+		} finally {
+			try { if (rs != null) rs.close(); } catch (SQLException e) { /* ignored */ }
+			try { if (pstmt != null) pstmt.close(); } catch (SQLException e) { /* ignored */ }
+			try { if (con != null) con.close(); } catch (SQLException e) { /* ignored */ }
+		}
+	}
+	
+	public Vector<Order> getOrders() {
+		return orders;
+	}
+
+	// TODO : 배송지 처리에 더 좋은 방법이 있는지 고민
+	// TODO : 테스트 필요
+	public void addOrder(Cart cart, String deliveryAddress) {
+		try {
+			LocalDate currentDate = LocalDate.now();
+			
+			con = DBConnector.getConnection();
+			pstmt = con.prepareStatement(String.join(" ", "INSERT INTO", OrderAttribute.TABLE_NAME, "VALUES (?,?,?,?,?, 1);"));
+			 
+			pstmt.setInt(1, orderID); 
+			pstmt.setString(2, cart.getuserID());
+			pstmt.setString(3, currentDate.toString());
+			pstmt.setString(4, deliveryAddress);
+			pstmt.setInt(5, cart.getTotalAmount());
+			
+			pstmt.executeUpdate();
+			
+			pstmt = con.prepareStatement(String.join(" ", "INSERT INTO", OrderListAttribute.TABLE_NAME, "VALUES (?,?,?);"));
+			for(Pair<Book, Integer, Integer> book : cart.getBooks()) {
+				
+				pstmt.setInt(1, orderID);
+				pstmt.setString(2, book.getFirst().getIsbn());
+				pstmt.setInt(3,  book.getSecond());
+				
+				pstmt.executeUpdate();
+			}
+			
+		} catch (SQLException ex) {
+			System.err.println("Database error in OrderService" + ex.getMessage());
+		} finally {
+			try { if (rs != null) rs.close(); } catch (SQLException e) { /* ignored */ }
+			try { if (pstmt != null) pstmt.close(); } catch (SQLException e) { /* ignored */ }
+			try { if (con != null) con.close(); } catch (SQLException e) { /* ignored */ }
+		}
+		orderID++;
+	}
+	public void loadValidOrders() {
+		orders.clear();
+		loadOrders(true);
+	}
+	
+	public void loadInvalidOrders() {
+		orders.clear();
+		loadOrders(false);
+	}
+	
+	public void loadAllOrders() {
+		orders.clear();
+		loadOrders(true);
+		loadOrders(false);
+	}
+	
+	// TODO : 테스트 필요
+	private void loadOrders(boolean isValid) {		
+		try {
+			Vector<String> isbns = new Vector<String>(); 
+			int orderID;
+			
+			ResultSet isbnRS;
+			con = DBConnector.getConnection();
+			
+			pstmt = con.prepareStatement(String.join(" ", "SELECT * FROM", OrderAttribute.TABLE_NAME, "WHERE", OrderAttribute.IS_VALID, "=?;"));			
+			pstmt.setBoolean(1, isValid);
+			
+			rs = pstmt.executeQuery();
+			while(rs.next()) {				
+				pstmt = con.prepareStatement(String.join(" ", "SELECT", OrderListAttribute.ISBN, "FROM", OrderListAttribute.TABLE_NAME, "WHERE", OrderListAttribute.ORDER_ID, "=?;"));
+				pstmt.setInt(1,  rs.getInt(OrderAttribute.ORDER_ID));
+				
+				isbnRS = pstmt.executeQuery();				
+				while(isbnRS.next()) {
+					isbns.add(isbnRS.getString(OrderListAttribute.ISBN));
+				}
+				
+				orders.add(new Order( rs.getInt(OrderAttribute.ORDER_ID)
+						, rs.getString(OrderAttribute.CUSTOMER_ID)
+						, rs.getString(OrderAttribute.ORDER_DATE)
+						, rs.getString(OrderAttribute.DELIVERY_ADDRESS)
+						, rs.getInt(OrderAttribute.TOTAL_AMOUNT)
+						, rs.getInt(OrderAttribute.TOTAL_BOOK_COUNT)
+						, isbns
+						, rs.getBoolean(OrderAttribute.IS_VALID))
+						);
+				isbns.clear();
+			}
+			
+		} catch (SQLException ex) {
+			System.err.println("Database error in OrderService" + ex.getMessage());
+			
+		} finally {
+			try { if (rs != null) rs.close(); } catch (SQLException e) { /* ignored */ }
+			try { if (pstmt != null) pstmt.close(); } catch (SQLException e) { /* ignored */ }
+			try { if (con != null) con.close(); } catch (SQLException e) { /* ignored */ }
+		}
+	}
 }
