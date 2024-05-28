70fe07049aed1247a0883d4f756e99b6e2ebbd25
Lee-Hacksun <hacksun530@hs.ac.kr>
2024-05-28 22:06:06 +0900

    검색기능 추가

---
+++ morningstar-library/src/control/SearchBookServlet.java
@@ -0,0 +1,56 @@
+package control;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.Book;
+import service.BookService;
+import service.InventoryService;
+
+/**
+ * Servlet implementation class SearchBookServlet
+ */
+@WebServlet("/SearchBook")
+public class SearchBookServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SearchBookServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String searchBookName = request.getParameter("searchBookName");
+		
+		InventoryService inventoryService = new InventoryService();
+		
+		HttpSession session = request.getSession();
+		BookService bookService = (BookService) session.getAttribute("bookService");
+
+		if (bookService == null) {
+		    bookService = new BookService();
+		    session.setAttribute("bookService", bookService);
+		}
+		Book findBook = bookService.searchBook(searchBookName);
+		
+		if(findBook == null) {
+			request.setAttribute("books", bookService.getBooks());
+			request.getRequestDispatcher("/mainpage.jsp").forward(request, response);
+		} else {
+			request.setAttribute("book", findBook);
+			request.setAttribute("price", inventoryService.getPrice(findBook.getIsbn()));
+			request.getRequestDispatcher("/book_detail.jsp").forward(request, response);
+		}
+	}
+
+}
--- morningstar-library/src/service/BookService.java
+++ morningstar-library/src/service/BookService.java
@@ -91,4 +91,39 @@ public class BookService {
 		}
 		return null;
 	}
+	
+	// TODO : 검색 도서가 하나 초과일때 처리 필요  
+	public Book searchBook(String searchBookName) {
+	      try {
+	         con = DBConnector.getConnection();
+	         pstmt = con.prepareStatement(String.join(" ", "SELECT * FROM", BookAttribute.TABLE_NAME, "WHERE", BookAttribute.BOOK_NAME, "LIKE ?;"));
+	         pstmt.setString(1, String.join("", "%", searchBookName, "%"));
+
+	         rs = pstmt.executeQuery();
+	         
+	         if(rs.next()) {
+	            return new Book(
+	                  rs.getString(BookAttribute.ISBN)
+	                  , rs.getString(BookAttribute.BOOK_NAME)
+	                  , rs.getString(BookAttribute.AUTHOR)
+	                  , rs.getString(BookAttribute.PUBLISHER)
+	                  , rs.getString(BookAttribute.RELEASE_DATE)
+	                  , rs.getString(BookAttribute.DESCRIPTION)
+	                  , rs.getString(BookAttribute.BOOK_IMG_URL)
+	                  , rs.getInt(BookAttribute.PAGE)
+	                  , rs.getInt(BookAttribute.CATEGORY));
+	         } else {
+	        	 return null;
+	         }
+	         
+	      } catch (SQLException ex) {
+	         System.err.println("Database error in BookService" + ex.getMessage()); 
+	         
+	      } finally {
+	         try { if (rs != null) rs.close(); } catch (SQLException e) { /* ignored */ }
+	         try { if (pstmt != null) pstmt.close(); } catch (SQLException e) { /* ignored */ }
+	         try { if (con != null) con.close(); } catch (SQLException e) { /* ignored */ }
+	      }
+	      return null;
+	   }
 }
